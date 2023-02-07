c97aabe4518c3395cc6c99ab835a2c2e445b1cd2
Cecilia Wambui <ceciliawambui026@gmail.com>
2023-02-07 16:14:28 +0300

    Sectioned the different parts of the portfolio

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,22 @@
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
+import "./app.scss"
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
 
 function App() {
   return (
     <div className="app">
-      Hello Portfolio. You empty!
+      {/* Hello Portfolio. You empty! */}
       <Topbar />
       <div className="sections">
         <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
       </div>
     </div>
   );
