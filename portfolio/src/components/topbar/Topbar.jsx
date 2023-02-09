574a98c946f68892f9d13600dfeb368ee339edb2
Cecilia Wambui <ceciliawambui026@gmail.com>
2023-02-09 16:59:32 +0300

    Worked on the scroll bar and the navbar

--- portfolio/src/components/topbar/Topbar.jsx
+++ portfolio/src/components/topbar/Topbar.jsx
@@ -3,8 +3,18 @@ import "./topbar.scss"
 
 function Topbar() {
   return (
-    <div className='topbar'>
-      Topbar
+    <div className='topbar' id="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#works" className="logo">genius.</a>
+          {/* <h1>Hello</h1> */}
+        </div>
+        <div className="right">
+          {/* This is right */}
+        </div>
+
+      </div>
+      
     </div>
   )
 }
