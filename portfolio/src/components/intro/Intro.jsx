870ab3024af9b71765d08221267fe69c0a40ce37
Cecilia Wambui <ceciliawambui026@gmail.com>
2023-02-22 16:50:54 +0300

    Simple Animated Typing

--- portfolio/src/components/intro/Intro.jsx
+++ portfolio/src/components/intro/Intro.jsx
@@ -1,7 +1,18 @@
 // import React from 'react'
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
 
 function intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, { 
+            showCursor: true,
+            strings:  ['Developer', 'Manager', 'Mentor'],
+        });
+    },[]);
+
     return (
         <div className='intro' id="intro">
             <div className="left">
@@ -13,13 +24,13 @@ function intro() {
                 <div className="wrapper">
                     <h2>Hi There, I'm</h2>
                     <h1>Skylar Williams</h1>
-                    <h3>Freelance <span>Software Developer</span></h3>
+                    <h3>Freelance <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                     <img src="assets/down4.png" alt="" />
                 </a>
             </div>
-            
+
         </div>
     )
 }
