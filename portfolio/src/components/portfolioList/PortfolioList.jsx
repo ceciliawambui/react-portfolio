3f4003b3757030177cb15a41a5822a2acfc8a189
Cecilia Wambui <ceciliawambui026@gmail.com>
2023-04-22 17:08:45 +0300

    Working on the portfolio

---
+++ portfolio/src/components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './portfolioList.scss'
+
+function PortfolioList() {
+  return (
+    <div>
+        <li className='portfolioList'></li>
+        
+    </div>
+  )
+}
+
+export default PortfolioList
